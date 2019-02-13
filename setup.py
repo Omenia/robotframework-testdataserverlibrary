8f88c3a89bdd9adcfe52db7cf726cab3dbdecda7
Juha Alhonen <ext-juha.alhonen@opr-finance.fi>
2019-02-13 13:30:50 +0200

    Copypasted initial setup.py from White-project

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup
+
+version_data = {}
+with open(os.path.join("version.py")) as f:
+    exec(f.read(), version_data)
+
+requirements = []
+with open("requirements.txt", "r") as f:
+    requirements = list(filter(lambda s: s!="", f.read().split("\n")))
+
+
+def __path(filename):
+    return os.path.join(os.path.dirname(__file__),
+                        filename)
+
+
+build = 0
+
+if os.path.exists(__path("build.info")):
+    build = open(__path("build.info")).read().strip()
+
+if version_data["STABLE"]:
+    version = "{}".format(version_data["VERSION"])
+else:
+    version = "{}.{}.pre".format(version_data["VERSION"], build)
+
+setup(name="robotframework-whitelibrary",
+      version=version,
+      description="Test Data Server library for Robot Framework",
+      author="SALabs",
+      author_email="to.be.added@noexist89a887.org",
+      url="https://github.com/Omenia/robotframework-testdataserverlibrary",
+      install_requires=requirements,
+      # packages=find_packages("src", exclude=["tests"]),
+      packages=
+      package_dir={"TDSlibrary": "TDSlibrary"},
+      classifiers=["Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6"],
+      )
