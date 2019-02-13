62fb51ec5f329b7dece1ddd9f1c75c26c5981177
Juha Alhonen <ext-juha.alhonen@opr-finance.fi>
2019-02-13 13:00:57 +0200

    Initial commit

---
+++ TDSlibrary.py
@@ -0,0 +1,28 @@
+from robot.libraries.BuiltIn import BuiltIn
+import requests
+
+class TDSlibrary:
+
+    def __init__(self, host='localhost', port=80):
+        self.port = port
+        self.host = host
+
+    def fetch_testdata(self, tablename):
+        uri = 'http://' + self.host + ':' + str(self.port) + '/api/v1/testdata/' + tablename
+        r = requests.get(uri)
+        BuiltIn().log(r.content)
+
+    def add_dataset(self):
+        pass
+
+    def remove_dataset(self):
+        pass
+
+    def add_data_to_dataset(self):
+        pass
+
+    def remove_data_from_dataset(self):
+        pass
+
+    def add_values_to_dataset(self):
+        pass
---
+++ __init__.py
@@ -0,0 +1,1 @@
+from TDSlibrary.TDSlibrary import TDSlibrary
