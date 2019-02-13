f268e882e2fb64dd699f406a59e4f48b24fe2b3b
Juha Alhonen <ext-juha.alhonen@opr-finance.fi>
2019-02-13 18:13:39 +0200

    REmoving version requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.0.6.pre'
+version = '0.0.7.pre'
 
 setup(name="robotframework-testdataserverlibrary",
       version=version,
@@ -9,7 +9,7 @@ setup(name="robotframework-testdataserverlibrary",
       author="SALabs",
       author_email="to.be.added@noexist89a887.org",
       url="https://github.com/Omenia/robotframework-testdataserverlibrary",
-      install_requires=['requests==2.21.0', 'robotframework==3.1'],
+      install_requires=['requests', 'robotframework'],
       py_modules=['TDSlibrary'],
       classifiers=["Programming Language :: Python :: 2",
                    "Programming Language :: Python :: 2.7",
--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-VERSION = '0.0.6.pre'
+VERSION = '0.0.7.pre'
 STABLE = False
 
 if __name__ == '__main__':
